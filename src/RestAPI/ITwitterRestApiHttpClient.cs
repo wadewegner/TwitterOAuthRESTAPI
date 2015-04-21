d3388faf2e339c33c8e295045a786400a4f17aa8
Aaron Hoffman <hoffman.aaron.d@gmail.com>
2015-04-21 13:26:12 -0500

    Add boilerplate rest api client

---
+++ src/RestAPI/ITwitterRestApiHttpClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterOAuth.RestAPI
+{
+    public interface ITwitterRestApiHttpClient
+    {
+        Task<T> GetAsync<T>(string resourceUri, dynamic parameters);
+    }
+}
---
+++ src/RestAPI/TwitterRestApiHttpClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterOAuth.RestAPI
+{
+    public class TwitterRestApiHttpClient : ITwitterRestApiHttpClient
+    {
+        private Authorization Authorization { get; set; }
+
+        public TwitterRestApiHttpClient(Authorization authorization)
+        {
+            this.Authorization = authorization;
+        }
+
+        public async Task<T> GetAsync<T>(string resourceUri, dynamic parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
